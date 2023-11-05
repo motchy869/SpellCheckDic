127f99f02ade42ceb675b691e47945a9e29076d5
motchy869 <motchy869@gmail.com>
2023-11-05 13:04:44 +0900

    perf: mroe efficient way

--- compile.sh
+++ compile.sh
@@ -2,10 +2,5 @@
 
 # This script searches all .txt files in "dicts" directory and concatenates them into one file "dict.txt"
 
-echo -n "" > dict.txt
-
 files=$(find ./dicts -name "*.txt")
-
-for file in $files; do
-    cat $file >> dict.txt
-done
+cat $files > dict.txt
