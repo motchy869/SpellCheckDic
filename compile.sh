33f230cf5d78d2de44a31512a8506dc98231d026
motchy <motchy869@gmail.com>
2024-05-03 23:45:36 +0900

    improve: avoid unintended word concatenation

--- compile.sh
+++ compile.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
 # This script searches all .txt files in "dicts" directory and concatenates them into one file "dict.txt"
+# To avoid unintended word concatenation, a line break is added at the end of each file.
 
 files=$(find ./dicts -name "*.txt")
-cat $files > dict.txt
+
+echo "" > dict.txt
+for file in $files; do
+    cat $file >> dict.txt
+    echo "" >> dict.txt
+done
+
+# Remove empty lines from dict.txt
+sed -i '/^$/d' dict.txt
